2260487e47b13b91b71725a4469a198738c8f1ae
Henry Schmid <henry.schmid1@gmail.com>
2022-01-04 16:44:35 -0700

    Create migrations for tea, customer, subscription models

---
+++ db/migrate/20220104234114_create_subscriptions.rb
@@ -0,0 +1,14 @@
+class CreateSubscriptions < ActiveRecord::Migration[5.2]
+  def change
+    create_table :subscriptions do |t|
+      t.belongs_to :customer, foreign_key: true
+      t.belongs_to :tea, foreign_key: true
+      t.string :title
+      t.float :price
+      t.integer :status
+      t.integer :frequency
+
+      t.timestamps
+    end
+  end
+end
