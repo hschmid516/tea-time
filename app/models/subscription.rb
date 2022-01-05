f5a31b0ef95f9b2a44a5632a1225b19295b68f35
Henry Schmid <henry.schmid1@gmail.com>
2022-01-04 21:14:32 -0700

    Add models and tests

---
+++ app/models/subscription.rb
@@ -0,0 +1,9 @@
+class Subscription < ApplicationRecord
+  belongs_to :customer
+  belongs_to :tea
+
+  validates :title, :price, :status, :frequency, presence: true
+
+  enum status: [:active, :cancelled]
+  enum frequency: [:daily, :weekly, :monthly]
+end
