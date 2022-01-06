d5ba028f23f6753cc4dfbc61a803afa04d892f80
Henry Schmid <henry.schmid1@gmail.com>
2022-01-06 16:24:59 -0700

    Add dependent destroy for customer

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,5 +1,5 @@
 class Customer < ApplicationRecord
-  has_many :subscriptions
+  has_many :subscriptions, dependent: :destroy
 
-  validates :first_name, :last_name, :email, :address, presence: true 
+  validates :first_name, :last_name, :email, :address, presence: true
 end
