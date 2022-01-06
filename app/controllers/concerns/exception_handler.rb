25a99e259a40d9c34b4a60b5910dd049c1e39c08
Henry Schmid <henry.schmid1@gmail.com>
2022-01-06 16:25:12 -0700

    Add bad request exception

--- app/controllers/concerns/exception_handler.rb
+++ app/controllers/concerns/exception_handler.rb
@@ -5,5 +5,9 @@ module ExceptionHandler
     rescue_from ActiveRecord::RecordNotFound do |e|
       render json: { errors: e.message }, status: :not_found
     end
+
+    rescue_from ActionController::BadRequest do |e|
+      render json: { errors: JSON.parse(e.message) }, status: :bad_request
+    end
   end
 end
