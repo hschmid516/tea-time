7e15fef51f35d0cb7603618835822a74ada76abe
Henry Schmid <henry.schmid1@gmail.com>
2022-01-06 16:25:29 -0700

    Refactor subscription controller to use services

--- app/controllers/api/v1/subscriptions_controller.rb
+++ app/controllers/api/v1/subscriptions_controller.rb
@@ -1,29 +1,23 @@
 class Api::V1::SubscriptionsController < ApplicationController
   def index
-    customer = Customer.find(params[:customer_id])
-
-    render json: SubscriptionSerializer.new(customer.subscriptions)
+    render json: SubscriptionSerializer.new(set_customer.subscriptions)
   end
 
   def create
-    subscription = Subscription.create(subscription_params)
-
-    if subscription.save
-      render json: SubscriptionSerializer.new(subscription)
-    else
-      render json: { errors: subscription.errors.full_messages }, status: 400
-    end
+    render json: Subscriber.call(subscription_params)
   end
 
   def update
-    subscription = Subscription.find(params[:id])
-    subscription.update(subscription_params)
-    render status: 204
+    render json: Activator.call(params)
   end
 
   private
 
   def subscription_params
     params.permit(:customer_id, :tea_id, :title, :price, :status, :frequency)
+  end
+
+  def set_customer
+    Customer.find(params[:customer_id])
   end
 end
